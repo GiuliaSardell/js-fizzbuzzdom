8f8378e9258444141164223375fecf705e65e83c
Giulia Sardell <giuliasardelli1234@gmail.com>
2021-10-26 10:57:06 +0200

    correzioni finali

--- js/script.js
+++ js/script.js
@@ -14,23 +14,29 @@ const row = document.querySelector('.row');
 for (let i = 1; i<=100; i++){
   const box = document.createElement('div');
   box.className = 'box';
-  box.innerHTML = i;
+  let result = i
+  
   row.append(box);
   console.log(box);
 
 
 if (!(i % 3) && (i % 5)) {
-  box.classList.add('per3');
+  result = 'fizz';
+  box.classList.add('fizz');
 }
 
 if (!(i % 5) && (i % 3)){
-  box.classList.add('per5');
+  result = 'buzz';
+  box.classList.add('buzz');
 }
 
 if (!(i % 3) && !(i % 5)){
-  box.classList.add('both');
+  result = 'fizzbuzz';
+  box.classList.add('fizzbuzz');
 }
 
+
+box.innerHTML = result;
 }
 
 
